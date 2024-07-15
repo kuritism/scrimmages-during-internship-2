353ba637b200402fec546eb7df6dc2520c7e669a
Samantha <109762632+kuritism@users.noreply.github.com>
2024-07-15 09:41:56 -0400

    bullet is bugged

--- level.gd
+++ level.gd
@@ -4,7 +4,8 @@ extends Node3D
 @onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
 
 const Player = preload("res://player.tscn")
-const PORT = 9998
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,22 +13,22 @@ func _process(delta):
 
 func _on_host_button_pressed():
 	main_menu.hide()
-	var enet_peer = ENetMultiplayerPeer.new()
+	
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
 	
 	add_player(multiplayer.get_unique_id())
+	
+	#upnp_setup()
 
 func _on_join_button_pressed():
 	main_menu.hide()
-	print("Yes")
-	var enet_peer = ENetMultiplayerPeer.new()
-	enet_peer.create_client("localhost", PORT)
-	if enet_peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		OS.alert("Failed to start multiplayer client.")
-		return
+	
+	enet_peer.create_client(address_entry.text, PORT)
 	multiplayer.multiplayer_peer = enet_peer
+
 	
 	
 func add_player(peer_id):
@@ -38,6 +39,27 @@ func add_player(peer_id):
 	print(player.name)
 	add_child(player)
 
+func remove_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+
 
 func _on_multiplayer_spawner_spawned(node):
 	print("bob")
+	
+#func upnp_setup():
+	#var upnp = UPNP.new()
+	#
+	#var discover_result = upnp.discover()
+	#assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		#"UPNP Discover Failed! Error %s" % discover_result)
+#
+	#assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		#"UPNP Invalid Gateway!")
+#
+	#var map_result = upnp.add_port_mapping(PORT)
+	#assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		#"UPNP Port Mapping Failed! Error %s" % map_result)
+	#
+	#print("Success! Join Address: %s" % upnp.query_external_address())
