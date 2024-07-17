810d66912e17cd14b9408b89f94d55db743bc0f2
M0xes2 <158515668+M0xes2@users.noreply.github.com>
2024-07-17 09:12:48 -0400

    YEAHHH GIMME SOME MONAY

---
+++ Spawner.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+@export var pickup: Resource
+var rng = RandomNumberGenerator.new()
+var current_pickup = null
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.start(rng.randi_range(5, 10))
+
+func _on_timer_timeout():
+	current_pickup = pickup.instantiate()
+	add_child(current_pickup)
+	current_pickup.position = Vector3(0, 1, 0)
+
+func _on_area_exited(area):
+	print("Body")
+	print(area)
+	print("curr Pick")
+	print(current_pickup)
+	if area == current_pickup:
+		print("New Pickup")
+		$Timer.start(rng.randi_range(20, 40))
