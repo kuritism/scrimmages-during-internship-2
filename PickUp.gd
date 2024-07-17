6d607bd7211cfac1865f88b42bff929b3e9509f9
M0xes2 <158515668+M0xes2@users.noreply.github.com>
2024-07-17 11:45:22 -0400

    fuish
    fdg dfg organik failure

--- PickUp.gd
+++ PickUp.gd
@@ -1,6 +1,15 @@
-extends MeshInstance3D
+extends Area3D
 
-func _on_area_3d_body_entered(body):
+@export var enchants: String
+func _ready():
+	var dir := DirAccess.open(enchants)
+	var file_names := dir.get_files()
+	var random_file = enchants + file_names[randi() % file_names.size()]
+	var enchant = load(random_file).instantiate()
+	$Enchant.add_child(enchant)
+	
+
+func _on_body_entered(body):
 	if body.has_method("on_enchant_pickup"):
 		body.on_enchant_pickup($Enchant.get_child(0))
 		queue_free()
